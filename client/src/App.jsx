d2c19844a661f5de312971f647dbdd967bd5b94c
masif2002 <masif2002@outlook.com>
2023-08-05 20:20:25 +0530

    Project Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { MdOutlineHotelClass } from "react-icons/md";
+import { GoSearch } from "react-icons/go";
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { BiSolidUserCircle } from 'react-icons/bi';
+
+function App() {
+  return (
+    <div>
+      Hi
+    </div>
+  );
+}
+
+export default App;
