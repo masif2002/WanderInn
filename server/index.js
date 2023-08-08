b83b335513ef6caf26a57a88e3a1df9b6670fce1
masif2002 <masif2002@outlook.com>
2023-08-08 07:26:34 +0530

    Add JWT authentication

--- server/index.js
+++ server/index.js
@@ -3,13 +3,16 @@ const cors = require('cors')
 const User = require('./models/User.js')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt') 
+const jwt = require('jsonwebtoken')
+
 require('dotenv').config()
 
 const app = express()
 
-const URL = process.env.MONGO_CONNECTION_URL
+const databaseURL = process.env.MONGO_CONNECTION_URL
+const jwtSecret = process.env.JWT_SECRET_KEY
 
-mongoose.connect(URL)
+mongoose.connect(databaseURL)
 
 app.use(express.json())
 
@@ -43,24 +46,33 @@ app.post('/register', async (req, res) => {
 })
 
 app.post('/login', async (req, res) => {
-  const { email, password } = req.body
-
+  
   try {
-    let user = await User.find({ email })
-   
 
-    if (user.length == 0) res.status(404).json({message: "Invalid Credentials" })
+    // Log in credentials
+    let { email, password } = req.body
 
+    // Fetch User
+    const user = await User.findOne({ email })
+    const { name, _id } = user
+
+    if (!user) {
+      return res.status(404).json({message: "Invalid Credentials" })
+    }
     else {
-      const passwordMatch = bcrypt.compareSync(password, user[0].password)
-      if (!passwordMatch) res.status(401).json({message: "Invalid Credentials"})
+      const passwordMatch = bcrypt.compareSync(password, user.password)
+      if (!passwordMatch) return res.status(401).json({message: "Invalid Credentials"})
     } 
 
-    res.json({message: "Autentication successful"})
+    // Sign JWT Token
+    jwt.sign({_id, name, email}, jwtSecret, function(err, token) {
+      if (err) throw err
+      return res.cookie('token', token).json({message: "Authentication successful"})
+    })
 
   } catch (e) {
     console.log(e);
-    res.status(404).json(e)
+    res.status(422).json({message: "Something went wrong! Please try again later"})
   }
 
 })
