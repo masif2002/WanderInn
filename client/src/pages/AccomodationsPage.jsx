8674027b4ccf4d3bf3e6e8da62df3131e2a050fc
masif2002 <masif2002@outlook.com>
2023-08-12 08:47:12 +0530

    Fix multiple uploads

--- client/src/pages/AccomodationsPage.jsx
+++ client/src/pages/AccomodationsPage.jsx
@@ -10,13 +10,29 @@ import axios from "axios";
 import { useState } from "react";
 
 
-const FormInput = ({ label, subText, textBox=false, placeholder, inputStyle }) => {
+const FormInput = ({ label, subText, textBox=false, placeholder, inputStyle, value, setValue }) => {
     return (
         <div className="mb-4">
             <label htmlFor="" className="font-semibold text-xl ml-3">{label}</label>
             <p className="ml-3 text-sm text-gray-400">{subText}</p>
-            {!textBox && <input type="text" className={inputStyle || "rounded-3xl"} placeholder={placeholder} />}
-            {textBox && <textarea className="rounded-lg" />}
+            {!textBox && 
+              <input 
+                type="text" 
+                className={inputStyle || "rounded-2xl"}
+                placeholder={placeholder} 
+                value={value}
+                onChange={(ev) => {
+                  setValue(ev.target.value)
+                }}
+              />}
+            {textBox && 
+              <textarea 
+                className="rounded-lg" 
+                value={value}
+                onChange={(ev) => {
+                  setValue(ev.target.value)
+                }}
+              />}
         </div>
 
     )
@@ -26,8 +42,19 @@ const AccomodationsPage = () => {
   const navigate = useNavigate();
   const { action } = useParams();
 
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [address, setAddress] = useState('')
+  const [perks, setPerks] = useState([])
+  const [extraInfo, setExtraInfo] = useState('')
+  const [checkIn, setCheckIn] = useState('')
+  const [checkOut, setCheckOut] = useState('')
+  const [maxGuests, setMaxGuests] = useState('')
+  
   const [photoLink, setPhotoLink] = useState('')
   const [uploadedPhotos, setUploadedPhotos] = useState([])
+
+
 
   const addPhotoByLink = (ev) => {
     ev.preventDefault()
@@ -47,20 +74,25 @@ const AccomodationsPage = () => {
 
   const uploadPhoto = async (ev) => {
     const { files } = ev.target
-   
-    const form = new FormData()
-    form.append('files', files[0])
+    console.log("Files", files)
 
-    axios.post('upload-photos', form)
-    .then(({ data }) => {
-      const newPhotos = data.map((URL) => axios.defaults.baseURL + URL)
-      console.log(newPhotos)
-      setUploadedPhotos((prev) => [...prev, ...newPhotos])
-    })
-    .catch((err) => {
-      console.log(err);
-      alert("Something went wrong!")
-    })
+    const form = new FormData()
+    
+    for (let i=0; i<files.length; i++) {
+      form.append('files', files[i])
+    }
+
+      axios.post('upload-photos', form)
+      .then(({ data }) => {
+        const newPhotos = data.map((URL) => axios.defaults.baseURL + URL)
+        console.log(newPhotos)
+        setUploadedPhotos((prev) => [...prev, ...newPhotos])
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Something went wrong!")
+      })
+
 
   }
 
@@ -80,9 +112,25 @@ const AccomodationsPage = () => {
         action == 'new' && (
             <form className="mt-10 py-5 px-10 max-w-3xl mx-auto mb-64">
                 
-                <FormInput label='Title' subText='Text that is displayed to the users on the front page' />
-                <FormInput label='Description' subText='Description of the place, the ambience, nearby stores, etc... ' textBox={true} />
-                <FormInput label='Address' subText='Address of the place and some nearby popular landmarks if any' />
+                <FormInput 
+                  label='Title' 
+                  subText='Text that is displayed to the users on the front page'
+                  value={title}
+                  setValue={setTitle}
+                />
+                <FormInput 
+                  label='Description' 
+                  subText='Description of the place, the ambience, nearby stores, etc... ' 
+                  textBox={true}
+                  value={description}
+                  setValue={setDescription}
+                />
+                <FormInput 
+                  label='Address'
+                  subText='Address of the place and some nearby popular landmarks if any'
+                  value={address}
+                  setValue={setAddress}
+                 />
 
                 {/* Photos */}
                 <div className="mb-4">
@@ -133,14 +181,41 @@ const AccomodationsPage = () => {
                 </div>
 
                 {/* Extrainfo */}
-                <FormInput label='Extra Info' subText='Any extra information that you would like your clients to know' textBox={true} />
+                <FormInput 
+                  label='Extra Info'
+                  subText='Any extra information that you would like your clients to know' 
+                  textBox={true}
+                  value={extraInfo}
+                  setValue={setExtraInfo}
+                 />
                 
                 {/* Check-in & Check-out */}
                 <div className="mb-4 grid grid-cols-3 gap-2">
 
-                    <FormInput label='Check-in' subText='Time to check in' placeholder='06:00' inputStyle={'rounded-lg'} />
-                    <FormInput label='Check-out' subText='Time to check out' placeholder='14:00' inputStyle={'rounded-lg'}/>
-                    <FormInput label='Max guests' subText='Maxium number of occupants' placeholder='3' inputStyle={'rounded-lg'}/>
+                    <FormInput 
+                      label='Check-in'
+                      subText='Time to check in' 
+                      placeholder='06:00' 
+                      inputStyle={'rounded-lg'} 
+                      value={checkIn}
+                      setValue={setCheckIn}
+                    />
+                    <FormInput 
+                      label='Check-out' 
+                      subText='Time to check out' 
+                      placeholder='14:00' 
+                      inputStyle={'rounded-lg'}
+                      value={checkOut}
+                      setValue={setCheckOut}
+                    />
+                    <FormInput 
+                      label='Max guests' 
+                      subText='Maxium number of occupants' 
+                      placeholder='3' 
+                      inputStyle={'rounded-lg'}
+                      value={maxGuests}
+                      setValue={setMaxGuests}
+                    />
 
                 </div>
 
